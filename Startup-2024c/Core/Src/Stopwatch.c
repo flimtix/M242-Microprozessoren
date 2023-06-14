27ad8f01e6e5bb8ad2579d2b25f8fda3cd41405b
21r8390 <21r8390@bztf.ch>
2023-06-14 22:28:40 +0200

    ✨: Stopwatch events

---
+++ Startup-2024c/Core/Src/Stopwatch.c
@@ -0,0 +1,116 @@
+/*-----------------------------------------------------------------------
+            D E F I N I T I O N    M O D U L E
+-------------------------------------------------------------------------
+
+PRODUCT:    Common Application (AP)
+
+TITLE:      Stopwatch
+
+PURPOSE:    Diese Datei ist für die Event-Steuerung der Stopwatch zuständig.
+
+------------------------------------------------------------------------*/
+
+//  -------------------------------------------------
+//  1. I N C L U D E S
+//  -------------------------------------------------
+
+#include "Stopwatch.h"
+
+//  -----------------------------------------------
+//  3.     I N T E R N A L    D E F I N I T I O N S
+//  -----------------------------------------------
+
+// The current state of the stopwatch
+static bool isStopwatchRunning = false;
+// The current state of the intermediate time
+static bool isShowingIntermediateTime = false;
+
+// The current time of the stopwatch in tenths of a second
+static unsigned int currentTime = 0;
+// The intermediate time of the stopwatch in tenths of a second
+static unsigned int intermediateTime = 0;
+
+//  -------------------------------------------
+//  4.     I N T E R N A L    F U N C T I O N S
+//  -------------------------------------------
+
+//  ---------------------------------------
+//  5.     G L O B A L    F U N C T I O N S
+//  ---------------------------------------
+
+// The background task to initialize the stopwatch
+void StopwatchTask(void *argument)
+{
+    while (true)
+    {
+        // Only increase the time if the stopwatch is running
+        if (IsStopwatchRunning())
+        {
+            // Increase the time by one tenth of a second
+            currentTime++;
+        }
+
+        // Increase the time every tenth of a second
+        osDelay(100);
+    }
+}
+
+// Starts the stopwatch
+void StartStopwatch()
+{
+    isStopwatchRunning = true;
+}
+
+// Stops the stopwatch
+void StopStopwatch()
+{
+    isStopwatchRunning = false;
+}
+
+// Resets the stopwatch
+void ResetStopwatch()
+{
+    isStopwatchRunning = false;
+    isShowingIntermediateTime = false;
+
+    currentTime = 0;
+    intermediateTime = 0;
+}
+
+// Shows the intermediate time
+void ShowIntermediateTime()
+{
+    intermediateTime = currentTime;
+    isShowingIntermediateTime = true;
+}
+
+// Hides the intermediate time
+void HideIntermediateTime()
+{
+    isShowingIntermediateTime = false;
+}
+
+// Returns either the current time or the intermediate time
+unsigned int GetStopwatchTime()
+{
+    if (isShowingIntermediateTime)
+    {
+        return intermediateTime;
+    }
+    else
+    {
+        return currentTime;
+    }
+}
+
+// Retuns if the stopwatch is running
+bool IsStopwatchRunning()
+{
+    return isStopwatchRunning;
+}
+
+// Returns if the intermediate time is showing
+bool IsShowingIntermediateTime()
+{
+    return isShowingIntermediateTime;
+}
---
+++ Startup-2024c/Core/Src/Stopwatch.h
@@ -0,0 +1,67 @@
+/*-----------------------------------------------------------------------
+            D E F I N I T I O N    M O D U L E
+-------------------------------------------------------------------------
+
+PRODUCT:    Common Application (AP)
+
+TITLE:      Stopwatch
+
+PURPOSE:    Diese Datei ist für die Event-Steuerung der Stopwatch zuständig.
+
+------------------------------------------------------------------------*/
+
+#ifndef _STOPWATCH
+#define _STOPWATCH
+
+//  -------------------------------------------------
+//  1. I N C L U D E S
+//  -------------------------------------------------
+
+#include "main.h"
+#include "cmsis_os.h"
+#include <stdbool.h>
+
+//  -------------------------------------------
+//  2.     G L O B A L    D E F I N I T I O N S
+//  -------------------------------------------
+
+//  ---------------------------------------
+//  3.     G L O B A L    C O N S T A N T S
+//  ---------------------------------------
+
+//  ---------------------------------------
+//  5.     G L O B A L    F U N C T I O N S
+//  ---------------------------------------
+
+// The background task to initialize the stopwatch
+// This will create a task for the stopwatch
+void StopwatchTask(void *argument);
+
+// Starts the stopwatch
+void StartStopwatch();
+
+// Stops the stopwatch
+void StopStopwatch();
+
+// Resets the stopwatch
+void ResetStopwatch();
+
+// Shows the intermediate time
+// The intermediate time is the time between the time when the method was called
+// The time will continue to run in the background
+void ShowIntermediateTime();
+
+// Hides the intermediate time
+void HideIntermediateTime();
+
+// Returns either the current time or the intermediate time
+// The time is in tenths of a second
+unsigned int GetStopwatchTime();
+
+// Returns true if the stopwatch is running
+bool IsStopwatchRunning();
+
+// Returns true if the stopwatch is showing the intermediate time
+bool IsShowingIntermediateTime();
+
+#endif
